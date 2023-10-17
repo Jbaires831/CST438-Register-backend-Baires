cc946807f9c8de5ab0b5198d7188a2b7fa905a8d
Jbaires831 <jbaires616@gmail.com>
2023-10-17 10:18:35 -0700

    implement assignment 5

--- src/test/java/com/cst438/EndToEndScheduleTest.java
+++ src/test/java/com/cst438/EndToEndScheduleTest.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testng.Assert.assertEquals;
 
 /*
  * This example shows how to use selenium testing using the web driver 
@@ -43,6 +44,21 @@ public class EndToEndScheduleTest {
 
 	public static final int SLEEP_DURATION = 1000; // 1 second.
 
+	public static final String TEST_ADD_NAME = "Test name";
+
+	public static final String TEST_ADD_EMAIL = "testname@gmail.com";
+
+	public static final String TEST_STATUS = "Active";
+
+	public static final int TEST_STATUS_CODE = 1;
+
+	public static final String TEST_UPDATE_NAME = "testName3";
+
+	public static final String TEST_UPDATE_EMAIL ="testname3@gmail.com";
+
+	public static final String TEST_UPDATE_STATUS = "Inactive";
+
+	public static final int Test_UPDATE_STATUSA_CODE = 0;
 
 	/*
 	 * add course TEST_COURSE_ID to schedule for 2021 Fall semester.
@@ -123,5 +139,124 @@ public class EndToEndScheduleTest {
 			driver.quit();
 		}
 
+	}
+
+	@Test
+	public void addStudentTest() throws Exception{
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		WebDriver driver = new ChromeDriver();
+		// Puts an Implicit wait for 10 seconds before throwing exception
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		try {
+			driver.get(URL);
+			// must have a short wait to allow time for the page to download
+			Thread.sleep(SLEEP_DURATION);
+
+			WebElement adminLink =
+					driver.findElement(By.xpath("//a[@href='/admin']"));
+			// go to admin page
+			adminLink.click();
+			Thread.sleep(SLEEP_DURATION);
+
+			// Locate and click "Add Student" button.
+			driver.findElement(By.id("addStudent")).click();
+			Thread.sleep(SLEEP_DURATION);
+
+			// enter student name, email, statusCode, and status. Then click Add button
+			driver.findElement(By.id("name")).sendKeys(TEST_ADD_NAME);
+			driver.findElement(By.id("email")).sendKeys(TEST_ADD_EMAIL);
+			driver.findElement(By.id("statusCode")).sendKeys(Integer.toString(TEST_STATUS_CODE));
+			driver.findElement(By.id("status")).sendKeys(TEST_STATUS);
+			driver.findElement(By.id("add")).click();
+			Thread.sleep(SLEEP_DURATION);
+
+			/*
+			 * verify that new student is in student table.
+			 * search for the student email in table.
+			 */
+			WebElement we = driver.findElement(By.xpath("//tr[td='"+TEST_ADD_EMAIL+"']"));
+			assertNotNull(we, "Test student email not found in table after successfully adding the course.");
+
+		} catch (Exception ex) {
+			throw ex;
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void UpdateStudentTest() throws Exception{
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		WebDriver driver = new ChromeDriver();
+		// Puts an Implicit wait for 10 seconds before throwing exception
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		try {
+			driver.get(URL);
+			// must have a short wait to allow time for the page to download
+			Thread.sleep(SLEEP_DURATION);
+
+			WebElement adminLink =
+					driver.findElement(By.xpath("//a[@href='/admin']"));
+			// go to admin page
+			adminLink.click();
+			Thread.sleep(SLEEP_DURATION);
+
+			// Locate correct student and click "Update Student" button.
+			driver.findElement(By.xpath("//tr[4]")).findElement(By.id("updateStudent")).click();
+			Thread.sleep(SLEEP_DURATION);
+
+			// enter new student name, email, statusCode, and status. Then click Add button
+			driver.findElement(By.id("name")).sendKeys(TEST_UPDATE_NAME);
+			driver.findElement(By.id("email")).sendKeys(TEST_UPDATE_EMAIL);
+			driver.findElement(By.id("statusCode")).sendKeys(Integer.toString(Test_UPDATE_STATUSA_CODE));
+			driver.findElement(By.id("status")).sendKeys(TEST_UPDATE_STATUS);
+			driver.findElement(By.id("update")).click();
+			Thread.sleep(SLEEP_DURATION);
+
+
+			WebElement we = driver.findElement(By.xpath("//tr[td='"+TEST_UPDATE_EMAIL+"']"));
+			assertNotNull(we, "Test student email not found in table after successfully updating the course.");
+
+			WebElement we2 = driver.findElement(By.xpath("//tr[td='"+TEST_UPDATE_NAME+"']"));
+			assertNotNull(we2, "Test student name not found in table after successfully updating the course.");
+
+		} catch (Exception ex) {
+			throw ex;
+		} finally {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void DeleteStudentTest() throws Exception{
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		WebDriver driver = new ChromeDriver();
+		// Puts an Implicit wait for 10 seconds before throwing exception
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		try {
+			driver.get(URL);
+			// must have a short wait to allow time for the page to download
+			Thread.sleep(SLEEP_DURATION);
+
+			WebElement adminLink =
+					driver.findElement(By.xpath("//a[@href='/admin']"));
+			// go to admin page
+			adminLink.click();
+			Thread.sleep(SLEEP_DURATION);
+
+			driver.findElement(By.xpath("//tr[4]")).findElement(By.id("delete")).click();
+			Thread.sleep(SLEEP_DURATION);
+
+			List<WebElement> listTd = driver.findElements(By.xpath("//td"));
+			assertEquals(listTd.size(), 3);
+
+		} catch (Exception ex) {
+			throw ex;
+		} finally {
+			driver.quit();
+		}
 	}
 }
